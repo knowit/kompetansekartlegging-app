1e561d688b76b77176f681db518c0bcbfafbcb41
Jan Henrik Gundelsby <janhenrik@gundelsby.com>
2022-02-25 15:06:47 +0100

    User for deployment

--- cdk/lib/KompetanseFrontendStack.ts
+++ cdk/lib/KompetanseFrontendStack.ts
@@ -2,9 +2,10 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
-import { KompetanseFrontendDistributionAndCertsStack } from './KompetanseFrontendDistributionAndCertsStaack';
+import { KompetanseFrontendDistributionAndCertsStack } from './KompetanseFrontendDistributionAndCertsStack';
 
 export class KompetanseFrontendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -28,6 +29,8 @@ export class KompetanseFrontendStack extends Stack {
       distributionPaths: ['/*'],
     });
 
+    const deployUser = new iam.User(this, 'deploy_website');
+    websiteBucket.grantWrite(deployUser);
 
     switch(process.env.DEPLOYMENT_ENV) { 
       case 'dev': { 
