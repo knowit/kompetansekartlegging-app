4fc48fc51db291537ffb3ad684cc55c8900203de
Kristian Stamland <kristian.stamland@knowit.no>
2022-03-04 16:22:51 +0100

    Added install script for backend and frontend

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo "Installing backend packages..."
+cd cdk && ./install.sh
+echo "Instaling frontend packages..."
+cd ../frontend && npm install
+echo "Instaling frontend cdk packages..."
+cd ../frontend-cdk && npm install
+echo "Finished installing packages"
