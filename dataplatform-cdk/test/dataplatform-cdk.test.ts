3fd04174ebe5a633074a1bd4c8ddffce3102247b
Kristian Stamland <kristian.stamland@knowit.no>
2022-03-21 13:32:25 +0100

    cdk init in dataplatform-cdk

---
+++ dataplatform-cdk/test/dataplatform-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DataplatformCdk from '../lib/dataplatform-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dataplatform-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DataplatformCdk.DataplatformCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
