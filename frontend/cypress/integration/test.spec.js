283e6fa6efc378ee288f0b5d920a68ca2b9fef57
Victoria Weber <victoria.weber@entur.org>
2022-09-14 09:37:22 +0200

    test

--- frontend/cypress/integration/test.spec.js
+++ frontend/cypress/integration/test.spec.js
@@ -4,7 +4,8 @@ Cypress.on("uncaught:exception", () => {
     return false;
 });
 
-
 it('lflf', () => {
     cy.visit("http://localhost:3000");
+    cy.get("button").contains("Dev login").click();
+
 })
