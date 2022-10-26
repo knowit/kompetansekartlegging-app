25455001abacf414119b73c0e8d293a2e87c254b
endremgs <endremg@stud.ntnu.no>
2022-10-26 14:02:11 +0200

    check for empty csv

--- cdk/backend/function/transformData/index.py
+++ cdk/backend/function/transformData/index.py
@@ -14,51 +14,60 @@ triggerFunctionName = environ.get("TRIGGER_FUNCTION")
 
 
 def handler(event, context):
-    transform("Organization",appendEmptyColumnsNames=["owner"])
+    transform("Organization", appendEmptyColumnsNames=["owner"])
     transform("User")
     transform("APIKeyPermission")
     transform("Category")
     transform("FormDefinition", removeColumns=["sortKeyConstant"])
     transform("Group")
-    transform("Question", removeColumns=["qid"]) #what is qid, is either empty or marked with <empty> on dynamoDB
+    # what is qid, is either empty or marked with <empty> on dynamoDB
+    transform("Question", removeColumns=["qid"])
     transform("QuestionAnswer", appendEmptyColumnsNames=["textValue"])
     transform("UserForm")
 
-    lambda_client.invoke(FunctionName=triggerFunctionName, 
-                     InvocationType='Event')
+    lambda_client.invoke(FunctionName=triggerFunctionName,
+                         InvocationType='Event')
+
 
 def getFileName(name):
     return f"{name}-{sourceName}-{env}.csv"
 
+
 def transform(name, removeColumns=[], appendEmptyColumnsNames=[]):
     key = getFileName(name)
     csvPdFile = getPandasCSVFile(key)
+
+    if len(csvPdFile) == 0:
+        print("File is empty, skipping insert")
+        return
+
     dropColumns(csvPdFile, removeColumns)
     if appendEmptyColumnsNames:
         csvPdFile = appendEmptyColumns(csvPdFile, appendEmptyColumnsNames)
     csvFile = csvPdFile.to_csv(index=False)
-    newFileKey = "transformed-"+ key
+    newFileKey = "transformed-" + key
     s3.Bucket(s3BucketTo).put_object(Key=newFileKey, Body=csvFile)
+
 
 def getPandasCSVFile(key):
     s3Object = s3Resource.Object(s3BucketFrom, key)
     fileContent = s3Object.get()["Body"].read().decode("utf-8")
     csvString = StringIO(fileContent)
-    return pd.read_csv(csvString, sep=",",header=[0])
-    
-def dropColumns(csvPdFile,colums = []):
+    try:
+        csv_content = pd.read_csv(csvString, sep=",", header=[0])
+        return csv_content
+    except Exception as e:
+        print(e)
+        print("Returning empty list")
+        return []
+
+
+def dropColumns(csvPdFile, colums=[]):
     dropColumns = ["__typename", "orgAdmins", "orgGroupLeaders"]
     dropColumns.extend(colums)
     dropColumns = csvPdFile.filter(dropColumns)
     csvPdFile.drop(dropColumns, inplace=True, axis=1)
 
+
 def appendEmptyColumns(csvPdFile, columns):
-    return csvPdFile.reindex([*csvPdFile.columns, *columns], axis ='columns', fill_value="")
-    
-
-
-
-
-
-
-
+    return csvPdFile.reindex([*csvPdFile.columns, *columns], axis='columns', fill_value="")
