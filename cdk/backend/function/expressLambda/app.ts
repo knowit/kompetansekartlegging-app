8ed360e975678969fb37dc759339868960f9ea33
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2022-11-16 13:16:13 +0100

    remove test, add debug, format

--- cdk/backend/function/expressLambda/app.ts
+++ cdk/backend/function/expressLambda/app.ts
@@ -1,30 +1,33 @@
-import express from "express";
-const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
-const bodyParser = require('body-parser');
+import express from "express"
+const awsServerlessExpressMiddleware = require("aws-serverless-express/middleware")
+const bodyParser = require("body-parser")
 
 const app = express()
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(awsServerlessExpressMiddleware.eventContext());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(awsServerlessExpressMiddleware.eventContext())
 
 // Enable CORS for all methods
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
 
 app.get("/", async (req, res) => {
-    try {
-        const response = {"data": "hei, det funket"}
-        res.status(200).json(response)
-    } catch (err) {
-        console.error(err)
-    }
+  try {
+    const response = { data: "hei, det funket" }
+    res.status(200).json(response)
+  } catch (err) {
+    console.error(err)
+  }
 })
 
-app.get("/test", (req, res) => {
-    return res.json({"message": "Dette funker jo dritbra!!"})
+app.get("/debug", (req, res) => {
+  return res.json({ message: "Buggin'" })
 })
 
-module.exports = app;
+module.exports = app
