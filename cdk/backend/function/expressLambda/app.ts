33b2cd676e86df25dd0be458c6fa5f2fff795f18
endremgs <endremg@stud.ntnu.no>
2022-11-30 13:34:16 +0100

    fix trailing comma

--- cdk/backend/function/expressLambda/app.ts
+++ cdk/backend/function/expressLambda/app.ts
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
+    "Origin, X-Requested-With, Content-Type, Accept"
   )
   next()
 })
