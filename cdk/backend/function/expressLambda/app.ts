d9e7e6de0c89dabe5f6fe03f02fcddeae3390dd1
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2022-11-30 12:52:38 +0100

    Add API router

--- cdk/backend/function/expressLambda/app.ts
+++ cdk/backend/function/expressLambda/app.ts
@@ -3,6 +3,7 @@ import {
   ExecuteStatementCommand,
   RDSDataClient,
 } from "@aws-sdk/client-rds-data"
+import { apiRouter } from "./api/router"
 
 const rds = new RDSDataClient({ region: "eu-central-1" })
 
@@ -10,9 +11,22 @@ const awsServerlessExpressMiddleware = require("aws-serverless-express/middlewar
 const bodyParser = require("body-parser")
 
 const app = express()
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(awsServerlessExpressMiddleware.eventContext())
+
+// Enable CORS for all methods
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
+
+app.use("/api", apiRouter)
 
 const cmdConstants = {
   resourceArn: process.env.DATABASE_ARN,
@@ -20,38 +34,13 @@ const cmdConstants = {
   database: process.env.DATABASE_NAME,
 }
 
-// Enable CORS for all methods
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
-  )
-  next()
-})
+export const sqlQuery = async (query: string) => {
+  const cmd = new ExecuteStatementCommand({
+    sql: query,
+    ...cmdConstants,
+  })
 
-app.get("/", async (req, res) => {
-  try {
-    const sqlString = "SELECT * FROM question"
-    const cmd = new ExecuteStatementCommand({
-      sql: sqlString,
-      ...cmdConstants,
-    })
+  return await rds.send(cmd)
+}
 
-    const dbResponse = await rds.send(cmd)
-
-    const response = { data: dbResponse }
-    res.status(200).json(response)
-  } catch (err) {
-    console.error(err)
-  }
-})
-
-app.get<unknown, unknown, unknown, { debugName: string }>(
-  "/debug",
-  (req, res) => {
-    return res.json({ name: req.query.debugName, message: "Buggin'" })
-  },
-)
-
-module.exports = app
+export default app
