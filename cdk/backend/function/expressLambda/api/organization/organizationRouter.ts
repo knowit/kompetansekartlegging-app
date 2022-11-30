83361508ce1b2276e2493fcda0b65e277ba294b6
Vidarmic <vidarmic@stud.ntnu.no>
2022-11-30 14:57:39 +0100

    updatedAt in schema and finalize /add in organization

--- cdk/backend/function/expressLambda/api/organization/organizationRouter.ts
+++ cdk/backend/function/expressLambda/api/organization/organizationRouter.ts
@@ -7,9 +7,7 @@ const router = express.Router()
 router.get("/list", async (req, res, next) => {
   try {
     const query = "SELECT id, orgname, identifierAttribute FROM organization"
-    const { records } = await sqlQuery(query)
-
-    const response = { data: records }
+    const response = await sqlQuery(query)
 
     res.status(200).json(response)
   } catch (err) {
@@ -30,7 +28,7 @@ router.post<unknown, unknown, addOrganizationParams>(
     const { id, orgname, identifierAttribute } = req.body
     try {
       const query =
-        "INSERT INTO organization VALUES(:id, :orgname, :identifierAttribute);"
+        "INSERT INTO organization VALUES(:id, DEFAULT, :owner, :orgname, :identifierAttribute);"
 
       const parameters: SqlParameter[] = [
         {
@@ -52,24 +50,31 @@ router.post<unknown, unknown, addOrganizationParams>(
           },
         },
         {
-          name: "createdAt",
+          name: "owner",
           value: {
-            stringValue: new Date().toISOString(),
-          },
-        },
+            stringValue: "TestOwner"
+          }
+        }
       ]
 
-      const { records } = await sqlQuery(query, parameters)
+      const response = await sqlQuery(query, parameters)
 
-      const response = { data: records }
       res.status(200).json(response)
     } catch (err) {
       next(err)
       console.error(err)
-    }
   }
-)
+})
 
-router.get("/remove")
+router.delete("/remove", async (req, res, next) =>{
+  try {
+    const query = "DELETE FROM organization WHERE id=:id;"
+    
+  } catch (err) {
+    next(err)
+    console.error(err)
+    
+  }
+})
 
 export { router as organizationRouter }
