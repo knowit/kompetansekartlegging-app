43b9070e26894d7edd37880dd5e2592f39bf3de3
Vidarmic <vidarmic@stud.ntnu.no>
2022-11-30 13:34:21 +0100

    addOrganization initial

--- cdk/backend/function/expressLambda/api/organization/organizationRouter.ts
+++ cdk/backend/function/expressLambda/api/organization/organizationRouter.ts
@@ -1,11 +1,12 @@
+import { SqlParameter } from "@aws-sdk/client-rds-data"
 import express from "express"
 import { sqlQuery } from "../../app"
 
 const router = express.Router()
 
-router.get("/listOrganizations", async (req, res, next) => {
+router.get("/list", async (req, res, next) => {
   try {
-    const query = "SELECT * FROM organization"
+    const query = "SELECT id, orgname, identifierAttribute FROM organization"
     const { records } = await sqlQuery(query)
 
     const response = { data: records }
@@ -16,5 +17,46 @@ router.get("/listOrganizations", async (req, res, next) => {
     console.error(err)
   }
 })
+
+interface addOrganization {
+  id: string,
+  orgname: string,
+  identifierAttribute: string
+}
+router.get<unknown, unknown, unknown, addOrganization>("/add", async (req, res, next) => {
+  const {id, orgname, identifierAttribute} = req.query
+  try {
+    const query = "INSERT INTO organization VALUES(:id, :orgname, :identifierAttribute);"
+    const parameters: SqlParameter[] = [
+      {
+        name: "id",
+        value: {
+          "stringValue": id
+        }
+      },
+      {
+        name: "orgname",
+        value: {
+          "stringValue": orgname
+        }
+      },
+      {
+        name: "identifierAttribute",
+        value: {
+          "stringValue": identifierAttribute
+        }
+      }
+    ]
+    const { records } = await sqlQuery(query, parameters)
+
+    const response = { data: records }
+    res.status(200).json(response)
+  } catch(err) {
+    next(err)
+    console.error(err)
+  }
+})
+
+router.get("/remove")
 
 export { router as organizationRouter }
