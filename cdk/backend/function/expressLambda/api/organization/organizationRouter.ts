ac198be23fe3f36898c0257771d1c812b5ed0827
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2022-11-30 14:12:01 +0100

    add createdAt

--- cdk/backend/function/expressLambda/api/organization/organizationRouter.ts
+++ cdk/backend/function/expressLambda/api/organization/organizationRouter.ts
@@ -18,44 +18,57 @@ router.get("/list", async (req, res, next) => {
   }
 })
 
-interface addOrganization {
-  id: string,
-  orgname: string,
+interface addOrganizationParams {
+  id: string
+  orgname: string
   identifierAttribute: string
 }
-router.get<unknown, unknown, unknown, addOrganization>("/add", async (req, res, next) => {
-  const {id, orgname, identifierAttribute} = req.query
-  try {
-    const query = "INSERT INTO organization VALUES(:id, :orgname, :identifierAttribute);"
-    const parameters: SqlParameter[] = [
-      {
-        name: "id",
-        value: {
-          "stringValue": id
-        }
-      },
-      {
-        name: "orgname",
-        value: {
-          "stringValue": orgname
-        }
-      },
-      {
-        name: "identifierAttribute",
-        value: {
-          "stringValue": identifierAttribute
-        }
-      }
-    ]
-    const { records } = await sqlQuery(query, parameters)
 
-    const response = { data: records }
-    res.status(200).json(response)
-  } catch(err) {
-    next(err)
-    console.error(err)
+router.post<unknown, unknown, addOrganizationParams>(
+  "/add",
+  async (req, res, next) => {
+    const { id, orgname, identifierAttribute } = req.body
+    try {
+      const query =
+        "INSERT INTO organization VALUES(:id, :orgname, :identifierAttribute);"
+
+      const parameters: SqlParameter[] = [
+        {
+          name: "id",
+          value: {
+            stringValue: id,
+          },
+        },
+        {
+          name: "orgname",
+          value: {
+            stringValue: orgname,
+          },
+        },
+        {
+          name: "identifierAttribute",
+          value: {
+            stringValue: identifierAttribute,
+          },
+        },
+        {
+          name: "createdAt",
+          value: {
+            stringValue: new Date().toISOString(),
+          },
+        },
+      ]
+
+      const { records } = await sqlQuery(query, parameters)
+
+      const response = { data: records }
+      res.status(200).json(response)
+    } catch (err) {
+      next(err)
+      console.error(err)
+    }
   }
-})
+)
 
 router.get("/remove")
 
