459841e18f273ab97aabcfd932116086985d8ee1
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2022-12-06 16:46:58 +0100

    Add endpoint to add/update user in group

--- cdk/backend/function/expressLambda/api/group/groupRouter.ts
+++ cdk/backend/function/expressLambda/api/group/groupRouter.ts
@@ -1,4 +1,4 @@
-import { SqlParameter } from '@aws-sdk/client-rds-data'
+import { SqlParameter, TypeHint } from '@aws-sdk/client-rds-data'
 import express from 'express'
 import { sqlQuery } from '../../app'
 
@@ -35,7 +35,7 @@ router.get<unknown, unknown, unknown, GetUsersInGroupParams>(
           value: {
             stringValue: groupId,
           },
-          typeHint: 'UUID',
+          typeHint: TypeHint.UUID,
         },
       ]
 
@@ -49,4 +49,85 @@ router.get<unknown, unknown, unknown, GetUsersInGroupParams>(
     }
   }
 )
+
+interface AddUserToGroupReqParams {
+  groupId: string
+}
+interface AddUserToGroupBodyParams {
+  userId: string
+  orgId: string
+}
+
+router.post<
+  unknown,
+  unknown,
+  AddUserToGroupBodyParams,
+  AddUserToGroupReqParams
+>('/:groupId/user/add', async (req, res, next) => {
+  try {
+    const { groupId } = req.query
+    const { userId, orgId } = req.body
+
+    // Check if user exists
+    const user = await sqlQuery('SELECT * FROM "user" WHERE id = :id', [
+      {
+        name: 'id',
+        value: {
+          stringValue: userId,
+        },
+      },
+    ])
+
+    const parameters: SqlParameter[] = [
+      {
+        name: 'id',
+        value: {
+          stringValue: userId,
+        },
+      },
+      {
+        name: 'groupid',
+        value: {
+          stringValue: groupId,
+        },
+        typeHint: TypeHint.UUID,
+      },
+      {
+        name: 'organizationid',
+        value: {
+          stringValue: orgId,
+        },
+      },
+    ]
+
+    // If user does not exist
+    if (user.length == 0) {
+      console.log('🚀 ~ > ~ User does not exist, creating new.')
+      const response = await sqlQuery(
+        'INSERT INTO "user" (id, groupid, organizationid) VALUES (:id, :groupid, :organizationid) RETURNING *',
+        parameters
+      )
+
+      return res.status(200).json({
+        message: '🚀 ~ > User did not exist, - created new.',
+        response,
+      })
+    }
+
+    // If user exists
+    console.log('🚀 ~ > ~ User does exist, updating.')
+    const response = await sqlQuery(
+      'UPDATE "user" SET groupid = :groupid, organizationid = :organizationid WHERE id=:id RETURNING *',
+      parameters
+    )
+
+    res
+      .status(200)
+      .json({ message: '🚀 ~ > User does exist, updated.', response })
+  } catch (err) {
+    console.error(err)
+    next(err)
+  }
+})
+
 export { router as groupRouter }
