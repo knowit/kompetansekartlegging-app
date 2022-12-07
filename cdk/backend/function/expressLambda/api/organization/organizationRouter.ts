34ea9ef3a667aa36f5162f465d78b808c19f38d6
Vidarmic <vidarmic@stud.ntnu.no>
2022-12-07 09:12:01 +0100

    Finalize organizationRouter

--- cdk/backend/function/expressLambda/api/organization/organizationRouter.ts
+++ cdk/backend/function/expressLambda/api/organization/organizationRouter.ts
@@ -1,15 +1,19 @@
-import { TypeHint } from '@aws-sdk/client-rds-data'
+import { SqlParameter } from '@aws-sdk/client-rds-data'
 import express from 'express'
 import { sqlQuery } from '../../app'
-import Organization from './organizationQueries'
 
 const router = express.Router()
 
 router.get('/list', async (_req, res, next) => {
   try {
-    const listOrganizationsResponse = await Organization.listOrganizations()
+    const SELECT_QUERY =
+      'SELECT id, orgname, identifierAttribute FROM organization'
+    const response = await sqlQuery(SELECT_QUERY)
 
-    res.status(200).json(listOrganizationsResponse)
+    res.status(200).json({
+      message: `🚀 ~ > All organizations selected.`,
+      response,
+    })
   } catch (err) {
     next(err)
     console.error(err)
@@ -27,6 +31,7 @@ router.post<unknown, unknown, addOrganizationParams>(
   '/add',
   async (req, res, next) => {
     const { id, orgname, identifierAttribute } = req.body
+    let owner = 'DefaultOwner'
     try {
       const addOrganizationResponse = await Organization.addOrganization(
         id,
@@ -34,7 +39,37 @@ router.post<unknown, unknown, addOrganizationParams>(
         identifierAttribute
       )
 
-      res.status(200).json(addOrganizationResponse)
+      const response = await sqlQuery(INSERT_QUERY, [
+        {
+          name: 'id',
+          value: {
+            stringValue: id,
+          },
+        },
+        {
+          name: 'orgname',
+          value: {
+            stringValue: orgname,
+          },
+        },
+        {
+          name: 'identifierAttribute',
+          value: {
+            stringValue: identifierAttribute,
+          },
+        },
+        {
+          name: 'owner',
+          value: {
+            stringValue: owner,
+          },
+        },
+      ])
+
+      res.status(200).json({
+        message: `🚀 ~ > Organization '${id}' is now in created with owner '${owner}'.`,
+        response,
+      })
     } catch (err) {
       next(err)
       console.error(err)
@@ -45,9 +80,19 @@ router.post<unknown, unknown, addOrganizationParams>(
 router.delete('/remove', async (req, res, next) => {
   const { id } = req.body
   try {
-    const removeOrganizationResponse = await Organization.removeOrganization(id)
-
-    res.status(200).json(removeOrganizationResponse)
+    const DELETE_QUERY = 'DELETE FROM organization WHERE id=:id;'
+    const response = await sqlQuery(DELETE_QUERY, [
+      {
+        name: 'id',
+        value: {
+          stringValue: id,
+        },
+      },
+    ])
+    res.status(200).json({
+      message: `🚀 ~ > Organization '${id}' is now deleted.`,
+      response,
+    })
   } catch (err) {
     next(err)
     console.error(err)
