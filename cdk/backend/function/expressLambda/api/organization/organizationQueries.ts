ef84037332d6e9d1b9722f5b656d892c63e58bad
Vidarmic <vidarmic@stud.ntnu.no>
2022-12-07 11:05:18 +0100

    Skeleton for organizationQueries

--- cdk/backend/function/expressLambda/api/organization/organizationQueries.ts
+++ cdk/backend/function/expressLambda/api/organization/organizationQueries.ts
@@ -1,44 +1,49 @@
-import { SqlParameter, TypeHint } from "@aws-sdk/client-rds-data"
-import { v4 as uuidv4 } from "uuid"
-import { sqlQuery } from "../../app"
+import { SqlParameter, TypeHint } from '@aws-sdk/client-rds-data'
+import { v4 as uuidv4 } from 'uuid'
+import { sqlQuery } from '../../app'
 
 const listOrganizations = async () => {
-    const query = "SELECT id, orgname, identifierAttribute FROM organization"
-    const records = await sqlQuery(query)
+  const query = 'SELECT id, orgname, identifierAttribute FROM organization'
+  const records = await sqlQuery(query)
 
-  return { message: "🚀 ~ > All organizations.", data: records }
+  return { message: '🚀 ~ > All organizations.', data: records }
 }
 
-const addOrganization = async (organizationId: string, orgname: string, identifierAttribute: string) => {
+const addOrganization = async (
+  organizationId: string,
+  orgname: string,
+  identifierAttribute: string
+) => {
   const params: SqlParameter[] = [
     {
-      name: "organizationID",
+      name: 'organizationID',
       value: {
         stringValue: organizationId,
       },
       typeHint: TypeHint.UUID,
     },
     {
-        name: "orgname",
-        value: {
-          "stringValue": orgname
-        }
+      name: 'orgname',
+      value: {
+        stringValue: orgname,
       },
-      {
-        name: "identifierAttribute",
-        value: {
-          "stringValue": identifierAttribute
-        }
+    },
+    {
+      name: 'identifierAttribute',
+      value: {
+        stringValue: identifierAttribute,
       },
-      {
-        name: "owner",
-        value: {
-          stringValue: owner,
-        },
-        typeHint: TypeHint.UUID,
-      }
+    },
+    {
+      name: 'owner',
+      value: {
+        stringValue: owner,
+      },
+      typeHint: TypeHint.UUID,
+    },
   ]
-  const query = "INSERT INTO organization(id, createdAt owner, orgname, identifierattribute) VALUES(:id, CAST(date '2020-10-10' AS TIMESTAMPTZ), DefaultOwner, :orgname, :identifierAttribute);"
+  const query =
+    "INSERT INTO organization(id, createdAt owner, orgname, identifierattribute) VALUES(:id, CAST(date '2020-10-10' AS TIMESTAMPTZ), DefaultOwner, :orgname, :identifierAttribute);"
   const records = await sqlQuery(query, params)
 
   return {
@@ -50,20 +55,20 @@ const addOrganization = async (organizationId: string, orgname: string, identifi
 const upsert = async (userId: string, groupId: string, orgId: string) => {
   const params: SqlParameter[] = [
     {
-      name: "id",
+      name: 'id',
       value: {
         stringValue: userId,
       },
     },
     {
-      name: "groupid",
+      name: 'groupid',
       value: {
         stringValue: groupId,
       },
       typeHint: TypeHint.UUID,
     },
     {
-      name: "organizationid",
+      name: 'organizationid',
       value: {
         stringValue: orgId,
       },
@@ -87,7 +92,7 @@ const upsert = async (userId: string, groupId: string, orgId: string) => {
 const deleteUser = async (userId: string) => {
   const params: SqlParameter[] = [
     {
-      name: "id",
+      name: 'id',
       value: {
         stringValue: userId,
       },
@@ -107,20 +112,20 @@ const createGroup = async (groupLeaderUsername: string, orgId: string) => {
 
   const params: SqlParameter[] = [
     {
-      name: "id",
+      name: 'id',
       value: {
         stringValue: generatedGroupId,
       },
       typeHint: TypeHint.UUID,
     },
     {
-      name: "organizationid",
+      name: 'organizationid',
       value: {
         stringValue: orgId,
       },
     },
     {
-      name: "groupleaderusername",
+      name: 'groupleaderusername',
       value: {
         stringValue: groupLeaderUsername,
       },
@@ -139,7 +144,7 @@ const createGroup = async (groupLeaderUsername: string, orgId: string) => {
 }
 
 export default {
-    listOrganizations,
+  listOrganizations,
   upsert,
   deleteUser,
   listUsersInGroup,
