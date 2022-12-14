7b17a0d99c0e3aae1bf1435c0ef4da6306fa483b
Christoffer Synnestvedt <56232993+CSynnestvedt@users.noreply.github.com>
2022-12-14 15:45:56 +0100

    Create endpoint for categories

--- cdk/backend/function/expressLambda/api/category/categoryRouter.ts
+++ cdk/backend/function/expressLambda/api/category/categoryRouter.ts
@@ -1,20 +1,96 @@
 import express from "express"
-import { sqlQuery } from "../../app"
+import Category from "./categoryQueries"
 
 const router = express.Router()
 
-router.get("/something", async (req, res, next) => {
+interface GetReqParams {
+  categoryId: string
+}
+
+// Get all categories
+router.get("/", async (req, res, next) => {
   try {
-    const query = "some query"
-    const { records } = await sqlQuery(query)
+    const listCategoriesResponse = await Category.listCategories()
 
-    const response = { data: records }
-
-    res.status(200).json(response)
+    res.status(200).json(listCategoriesResponse)
   } catch (err) {
-    next(err)
     console.error(err)
   }
 })
+
+// Get single category from id
+router.get<GetReqParams, unknown, unknown, unknown>(
+  "/:categoryId",
+  async (req, res, next) => {
+    try {
+      const { categoryId } = req.params
+      const getCategoryResponse = await Category.getCategory(categoryId)
+
+      res.status(200).json(getCategoryResponse)
+    } catch (err) {
+      console.error(err)
+      next(err)
+    }
+  }
+)
+
+interface createCategoryBodyParams {
+  id: string
+  text: string
+  description: string
+  index: number
+  formDefinitionId: string
+  organizationId: string
+}
+
+// Create category
+router.post<unknown, unknown, createCategoryBodyParams, unknown>(
+  "/",
+  async (req, res, next) => {
+    try {
+      const createCategoryResponse = await Category.createCategory(req.body)
+
+      res.status(201).json(createCategoryResponse)
+    } catch (err) {
+      console.error(err)
+      next(err)
+    }
+  }
+)
+
+// Update category with given id
+router.patch<GetReqParams, unknown, createCategoryBodyParams, unknown>(
+  "/:categoryId",
+  async (req, res, next) => {
+    try {
+      const updateCategoryResponse = await Category.updateCategory({
+        ...req.body,
+        ...req.params,
+      })
+
+      res.status(200).json(updateCategoryResponse)
+    } catch (err) {
+      console.error(err)
+      next(err)
+    }
+  }
+)
+
+// Delete category with given id
+router.delete<GetReqParams, unknown, unknown, unknown>(
+  "/:categoryId",
+  async (req, res, next) => {
+    try {
+      const deleteCategoryResponse = await Category.deleteCategory(
+        req.params.categoryId
+      )
+
+      res.status(200).json(deleteCategoryResponse)
+    } catch (err) {
+      console.error(err)
+      next(err)
+    }
+  }
+)
 
 export { router as categoryRouter }
