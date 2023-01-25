08719469833dfa3563db632070131d15b4bef282
Christoffer Synnestvedt <56232993+CSynnestvedt@users.noreply.github.com>
2023-01-25 10:16:26 +0100

    Set up endpoint for questionAnswers and questions (#49)
    * Set up endpoint for questionAnswer
    
    * Create enpoints for questions
    
    * Add todo comment

---
+++ cdk/backend/function/expressLambda/api/questions/questionsQuery.ts
@@ -0,0 +1,236 @@
+import { SqlParameter, TypeHint } from '@aws-sdk/client-rds-data'
+import { sqlQuery } from '../../app'
+
+import { v4 as uuidv4 } from 'uuid'
+
+export interface CreateQuestionProps {
+  categoryId: string
+  formDefinitionId: string
+  index: number
+  organizationId: string
+  scaleStart: string | undefined
+  scaleMiddle: string | undefined
+  scaleEnd: string | undefined
+  text: string
+  topic: string
+  type2: QuestionType
+  type: 'customScaleValue' | 'knowledgeMotivation' | 'text'
+}
+
+export enum QuestionType {
+  customScaleValue = 'customScaleValue',
+  knowledgeMotivation = 'knowledgeMotivation',
+  text = 'text',
+}
+
+const listQuestions = async () => {
+  const query = `SELECT * FROM question`
+  const response = await sqlQuery(query)
+
+  return {
+    message: '🚀 ~ > All questions',
+    data: response,
+  }
+}
+
+const getQuestion = async (id: string) => {
+  const params: SqlParameter[] = [
+    {
+      name: 'id',
+      value: {
+        stringValue: id,
+      },
+      typeHint: TypeHint.UUID,
+    },
+  ]
+
+  const query = `SELECT * FROM question WHERE id = :id`
+  const response = await sqlQuery(query, params)
+
+  return {
+    message: `🚀 ~ > Question with id: ${id}`,
+    data: response,
+  }
+}
+
+const getQuestionsInCategory = async (categoryId: string) => {
+  const params: SqlParameter[] = [
+    {
+      name: 'categoryId',
+      value: {
+        stringValue: categoryId,
+      },
+      typeHint: TypeHint.UUID,
+    },
+  ]
+
+  const query = `SELECT * FROM question WHERE categoryID = :categoryId`
+  const response = await sqlQuery(query, params)
+
+  return {
+    message: `🚀 ~ > All questions with categoryId: ${categoryId}`,
+    data: response,
+  }
+}
+
+const createQuestion = async ({
+  categoryId,
+  formDefinitionId,
+  index,
+  organizationId,
+  scaleStart,
+  scaleMiddle,
+  scaleEnd,
+  text,
+  topic,
+  type,
+}: CreateQuestionProps) => {
+  const id = uuidv4()
+
+  const params: SqlParameter[] = [
+    {
+      name: 'id',
+      value: {
+        stringValue: id,
+      },
+      typeHint: TypeHint.UUID,
+    },
+    {
+      name: 'categoryId',
+      value: {
+        stringValue: categoryId,
+      },
+      typeHint: TypeHint.UUID,
+    },
+    {
+      name: 'formDefinitionId',
+      value: { stringValue: formDefinitionId },
+      typeHint: TypeHint.UUID,
+    },
+    { name: 'index', value: { longValue: index } },
+    { name: 'organizationId', value: { stringValue: organizationId } },
+    {
+      name: 'scaleStart',
+      value: scaleStart ? { stringValue: scaleStart } : { isNull: true },
+    },
+    {
+      name: 'scaleMiddle',
+      value: scaleMiddle ? { stringValue: scaleMiddle } : { isNull: true },
+    },
+    {
+      name: 'scaleEnd',
+      value: scaleEnd ? { stringValue: scaleEnd } : { isNull: true },
+    },
+    { name: 'text', value: { stringValue: text } },
+    { name: 'topic', value: { stringValue: topic } },
+  ]
+
+  const questionType = QuestionType[type]
+
+  const query = `INSERT INTO "question" (id, categoryID, formDefinitionID, index, organizationID, scaleStart, scaleMiddle, scaleEnd, text, topic, type)
+    VALUES(:id, :categoryId, :formDefinitionId, :index, :organizationId, :scaleStart, :scaleMiddle, :scaleEnd, :text, :topic, '${questionType}')
+    RETURNING *`
+
+  const response = await sqlQuery(query, params)
+
+  return {
+    message: `🚀 ~ > Question with id: ${id} was successfully created`,
+    data: response,
+  }
+}
+
+const updateQuestion = async (
+  id: string,
+  {
+    categoryId,
+    formDefinitionId,
+    index,
+    organizationId,
+    scaleStart,
+    scaleMiddle,
+    scaleEnd,
+    text,
+    topic,
+    type,
+  }: CreateQuestionProps
+) => {
+  const params: SqlParameter[] = [
+    {
+      name: 'id',
+      value: {
+        stringValue: id,
+      },
+      typeHint: TypeHint.UUID,
+    },
+    {
+      name: 'categoryId',
+      value: {
+        stringValue: categoryId,
+      },
+      typeHint: TypeHint.UUID,
+    },
+    {
+      name: 'formDefinitionId',
+      value: { stringValue: formDefinitionId },
+      typeHint: TypeHint.UUID,
+    },
+    { name: 'index', value: { longValue: index } },
+    { name: 'organizationId', value: { stringValue: organizationId } },
+    {
+      name: 'scaleStart',
+      value: scaleStart ? { stringValue: scaleStart } : { isNull: true },
+    },
+    {
+      name: 'scaleMiddle',
+      value: scaleMiddle ? { stringValue: scaleMiddle } : { isNull: true },
+    },
+    {
+      name: 'scaleEnd',
+      value: scaleEnd ? { stringValue: scaleEnd } : { isNull: true },
+    },
+    { name: 'text', value: { stringValue: text } },
+    { name: 'topic', value: { stringValue: topic } },
+  ]
+
+  const questionType = QuestionType[type]
+
+  const query = `UPDATE "question"
+    SET categoryID=:categoryId, formDefinitionID=:formDefinitionId, index=:index,
+    organizationID=:organizationId, scaleStart=:scaleStart, scaleMiddle=:scaleMiddle, scaleEnd=:scaleEnd,
+    text=:text, topic=:topic, type='${questionType}'
+    WHERE id=:id
+    RETURNING *`
+
+  const response = await sqlQuery(query, params)
+
+  return {
+    message: `🚀 ~ > Question with id: ${id} was updated`,
+    data: response,
+  }
+}
+
+const deleteQuestion = async (id: string) => {
+  const params: SqlParameter[] = [
+    {
+      name: 'id',
+      value: { stringValue: id },
+      typeHint: TypeHint.UUID,
+    },
+  ]
+
+  const query = `DELETE FROM "question" WHERE id=:id RETURNING *`
+  const response = await sqlQuery(query, params)
+
+  return {
+    message: `🚀 ~ > Question with id: ${id} was successfully deleted`,
+  }
+}
+
+export default {
+  listQuestions,
+  getQuestion,
+  getQuestionsInCategory,
+  createQuestion,
+  updateQuestion,
+  deleteQuestion,
+}
--- cdk/backend/function/expressLambda/api/router.ts
+++ cdk/backend/function/expressLambda/api/router.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import { categoryRouter } from './category/categoryRouter'
 import { debugRouter } from './debug/debugRouter'
-import { formDefinitionRouter } from './formDefinition/formDefinitionRouter'
 import { groupRouter } from './group/groupRouter'
 import { organizationRouter } from './organization/organizationRouter'
-import { questionAnswerRouter } from './questionAnswer/questionAnswerRouter'
+import { questionsRouter } from './questions/questionsRouter'
+import { questionAnswersRouter } from './questionAnswers/questionAnswersRouter'
+import { formDefinitionRouter } from './formDefinition/formDefinitionRouter'
 
 const router = express.Router()
 
@@ -12,7 +13,8 @@ router.use('/debug', debugRouter)
 router.use('/organizations', organizationRouter)
 router.use('/groups', groupRouter)
 router.use('/categories', categoryRouter)
-router.use('/questionAnswer', questionAnswerRouter)
+router.use('/questionAnswers', questionAnswersRouter)
+router.use('/questions', questionsRouter)
 router.use('/formDefinitions', formDefinitionRouter)
 
 export { router as apiRouter }
