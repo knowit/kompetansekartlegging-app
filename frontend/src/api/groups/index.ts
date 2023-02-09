9126049c8acd06eeb7712e0f884d770c48c4c45a
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-02-09 22:35:44 +0100

    Finish API for groups and users frontend and backend

--- frontend/src/api/groups/index.ts
+++ frontend/src/api/groups/index.ts
@@ -12,35 +12,32 @@ import {
 
 export const getAllGroups = async () => apiGET<Group[]>('/groups')
 
-export const getAllUsersInGroup = async ({ groupid }: GetUsersInput) =>
-  apiGET<User[]>('/groups/users', { queryStringParameters: { groupid } })
+export const getAllUsersInGroup = async (groupid: GetUsersInput) =>
+  apiGET<User[]>('/groups/users', { queryStringParameters: groupid })
 
 export const addUserToGroup = async (
-  { id: groupid }: GetGroupInput,
-  { id, organizationid }: AddUserInput
+  id: GetGroupInput,
+  userData: AddUserInput
 ) =>
   apiPOST<User>('/groups/:id/user', {
-    queryStringParameters: { id: groupid },
-    body: { id, organizationid },
+    queryStringParameters: id,
+    body: userData,
   })
 
-export const deleteUserFromGroup = async ({ id }: DeleteUserInput) =>
-  apiDELETE<User>('/groups/user', { body: { id } })
+export const deleteUserFromGroup = async (id: DeleteUserInput) =>
+  apiDELETE<User>('/groups/user', { body: id })
 
-export const createGroup = async ({
-  organizationid,
-  groupleaderusername,
-}: GroupInput) =>
-  apiPOST<Group>('/groups', { body: { organizationid, groupleaderusername } })
+export const createGroup = async (data: GroupInput) =>
+  apiPOST<Group>('/groups', { body: data })
 
 export const updateGroupLeader = async (
-  { id }: GetGroupInput,
-  { groupleaderusername }: UpdateGroupLeaderInput
+  id: GetGroupInput,
+  data: UpdateGroupLeaderInput
 ) =>
   apiPATCH<Group>('/groups', {
-    queryStringParameters: { id },
-    body: { groupleaderusername },
+    queryStringParameters: id,
+    body: data,
   })
 
-export const deleteGroup = async ({ id }: DeleteGroupInput) =>
-  apiDELETE<Group>('/groups', { body: { id } })
+export const deleteGroup = async (id: DeleteGroupInput) =>
+  apiDELETE<Group>('/groups', { body: id })
