f351d911c5338d63f2c13c567b7709077e584d5f
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-02-14 21:34:07 +0100

    Finish API for formDefinition frontend and backend

--- frontend/src/components/AdminPanel/AdminPanel.tsx
+++ frontend/src/components/AdminPanel/AdminPanel.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
+import { getAllQuestionAnswers } from '../../api/questionAnswers'
 
-import { deleteCategory } from '../../api/categories'
 import style from './AdminPanel.module.css'
 import EditAdmins from './EditAdmins'
 import EditCatalogsRouter from './EditCatalogsRouter'
@@ -41,37 +41,6 @@ const activeSubmenuItemToSubmenuCategory = (
 
 const AdminPanel = ({ activeSubmenuItem }: AdminPanelProps) => {
   const category = activeSubmenuItemToSubmenuCategory(activeSubmenuItem)
-
-  useEffect(() => {
-    const fetchApi = async () => {
-      // const res = await getCategoryById('af3764b5-5d9f-448c-beb1-7e8c7d8e80e5')
-      // const res = await apiDELETE('/categories/:categoryId', {
-      //   queryStringParameters: {
-      //     categoryId: CAT_ID,
-      //   },
-      // })
-      //const res = await getAllCategories()
-      // const res = await getAllUsersInGroup(
-      //   'f861818a-8019-4207-b8c9-026404d988e5'
-      // )
-      // const res = await createGroup({
-      //   groupleaderusername: 'testing123',
-      //   orgid: 'knowitobjectnet',
-      // })
-      // const res = await addUserToGroup('826d06f9-9d84-4cfe-94d1-901ccc4988c9', {
-      //   id: 'testing123',
-      //   orgid: 'knowitobjectnet',
-      // })
-      // const res = await updateGroupLeader(
-      //   '826d06f9-9d84-4cfe-94d1-901ccc4988c9',
-      //   'testing123'
-      // )
-      // const res = await deleteGroup('7dac7072-2452-46bb-9913-d8df4140399b')
-      // const res = await deleteUserFromGroup('testing123')
-    }
-
-    fetchApi()
-  }, [])
 
   return (
     <div className={style.container}>
