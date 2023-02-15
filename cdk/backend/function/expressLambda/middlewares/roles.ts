714278de0a275b5fb07e85056a2a550ce61029cd
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-02-15 12:49:42 +0100

    Add role middleware

--- cdk/backend/function/expressLambda/middlewares/roles.ts
+++ cdk/backend/function/expressLambda/middlewares/roles.ts
@@ -1,12 +1,28 @@
 import { NextFunction, Request, Response } from 'express'
 
-export const requireRole = (role: any) => {
+export enum Role {
+  ADMIN = 'admin',
+  GROUP_LEADER = 'groupleader',
+}
+
+export const getRoles = (req: Request) =>
+  req.apiGateway.event.requestContext.authorizer?.claims['cognito:groups']
+    .split(',')
+    .map((r: string) => {
+      const role = r.split('0')
+      if (role.length > 1) {
+        return role[1].toLocaleLowerCase()
+      }
+    })
+
+export const requireRole = (role: Role) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const { user } = req.query
-    if (user && user.roles.includes(role)) {
+    const userRoles = getRoles(req)
+
+    if (userRoles.includes(role)) {
       next()
     } else {
-      res.status(403).send('Access Denied')
+      res.status(403).send(`Access Denied: ${role} required`)
     }
   }
 }
