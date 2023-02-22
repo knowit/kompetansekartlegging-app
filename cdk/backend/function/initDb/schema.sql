970c97dd5de6b2d039ad84e38ed460e0a3c3c369
Christoffer Synnestvedt <christoffer.synnestvedt@gmail.com>
2023-02-22 10:03:05 +0100

    Fix sql errors

--- cdk/backend/function/initDb/schema.sql
+++ cdk/backend/function/initDb/schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS category(
     text VARCHAR(255) NOT NULL,
     description TEXT,
     index INTEGER,
-    catalog_id UUID NOT NULL references "catalog"(id),
+    catalog_id UUID NOT NULL references "catalog"(id)
 );
 DO $$ BEGIN CREATE TYPE question_type AS ENUM (
     'knowledge_motivation',
@@ -42,7 +42,12 @@ CREATE TABLE IF NOT EXISTS question(
     scale_start VARCHAR(255),
     scale_middle VARCHAR(255),
     scale_end VARCHAR(255),
-    category_id UUID NOT NULL references category(id),
+    category_id UUID NOT NULL references category(id)
+);
+CREATE TABLE IF NOT EXISTS "user"(
+    id UUID PRIMARY KEY NOT NULL,
+    mail VARCHAR(255) NOT NULL UNIQUE,
+    organization_id VARCHAR(255) NOT NULL references organization(id)
 );
 CREATE TABLE IF NOT EXISTS question_answer(
     id UUID PRIMARY KEY NOT NULL,
@@ -56,11 +61,7 @@ CREATE TABLE IF NOT EXISTS question_answer(
 CREATE TABLE IF NOT EXISTS "group"(
     id UUID PRIMARY KEY NOT NULL,
     organization_id VARCHAR(255) NOT NULL references organization(id),
-    group_leader_id VARCHAR(255) NOT NULL references "user"(id)
 );
-CREATE TABLE IF NOT EXISTS "user"(
-    id UUID PRIMARY KEY NOT NULL,
-    mail VARCHAR(255) NOT NULL UNIQUE,
-    group_id UUID references "group"(id),
-    organization_id VARCHAR(255) NOT NULL references organization(id)
-);
+ALTER TABLE "user" ADD IF NOT EXISTS group_id UUID references "group"(id);
+ALTER TABLE "group" ADD IF NOT EXISTS group_leader_id UUID NOT NULL references "user"(id);
+
