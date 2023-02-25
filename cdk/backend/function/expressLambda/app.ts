08965b6af71a22ec4f417e3dcaa91c3b3840388f
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-02-25 11:41:56 +0100

    Revert funky random @CSynnestvedt edit hehe

--- cdk/backend/function/expressLambda/app.ts
+++ cdk/backend/function/expressLambda/app.ts
@@ -1,24 +1,18 @@
 import {
   ExecuteStatementCommand,
-  Field,
   RDSDataClient,
   RecordsFormatType,
   SqlParameter,
 } from '@aws-sdk/client-rds-data'
 import express from 'express'
 import { apiRouter } from './api/router'
-
 const rds = new RDSDataClient({ region: 'eu-central-1' })
-
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 const bodyParser = require('body-parser')
-
 const app = express()
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(awsServerlessExpressMiddleware.eventContext())
-
 // Enable CORS for all methods
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
@@ -28,31 +22,18 @@ app.use((req, res, next) => {
   )
   next()
 })
-
 // Add all API routerse
 app.use('/api', apiRouter)
-
 const cmdConstants = {
   resourceArn: process.env.DATABASE_ARN,
   secretArn: process.env.SECRET_ARN,
   database: process.env.DATABASE_NAME,
 }
-
 interface SqlQueryInput {
   query: string
   parameters?: SqlParameter[]
   message: string
 }
-// Enable CORS for all methods
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
-})
-
 export const sqlQuery = async ({
   query,
   parameters,
@@ -64,10 +45,8 @@ export const sqlQuery = async ({
     ...cmdConstants,
     formatRecordsAs: RecordsFormatType.JSON,
   })
-
   const response = await rds.send(cmd)
   console.log('🚀 ~ response', response)
-
   if (response.formattedRecords) {
     const records = JSON.parse(response.formattedRecords)
     if (records.length === 0) {
@@ -76,7 +55,6 @@ export const sqlQuery = async ({
         data: null,
       }
     }
-
     return {
       message,
       data: records.length === 1 ? records[0] : records,
@@ -85,47 +63,4 @@ export const sqlQuery = async ({
     throw new Error(`Something went wrong in sqlQuery(...)`)
   }
 }
-
-app.get('/categories', async (req, res) => {
-  try {
-    const sqlString = 'SELECT * FROM category'
-    const cmd = new ExecuteStatementCommand({
-      sql: sqlString,
-      ...cmdConstants,
-    })
-
-    const dbResponse = await rds.send(cmd)
-
-    const response = { data: dbResponse }
-    res.status(200).json(response)
-  } catch (err) {
-    console.error(err)
-  }
-})
-
-app.get('/categories/:id/questions', async (req, res) => {
-  const id = req.params.id
-  try {
-    const sqlString = `SELECT * FROM question WHERE categoryID=?`
-    const cmd = new ExecuteStatementCommand({
-      sql: sqlString,
-      ...cmdConstants,
-    })
-
-    const dbResponse = await rds.send(cmd)
-
-    const response = { data: dbResponse }
-    res.status(200).json(response)
-  } catch (err) {
-    console.error(err)
-  }
-})
-
-app.get<unknown, unknown, unknown, { debugName: string }>(
-  '/debug',
-  (req, res) => {
-    return res.json({ name: req.query.debugName, message: "Buggin'" })
-  }
-)
-
-module.exports = app
+export default app
