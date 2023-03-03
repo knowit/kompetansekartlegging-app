cb2ac985ed7b4bd6f9e64b5c375c9b66e93289df
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-03-03 12:41:11 +0100

    Make cdk pretty

--- cdk/hooks/hooks.ts
+++ cdk/hooks/hooks.ts
@@ -1,25 +1,37 @@
-import setAppSyncAuth from "./setAppSyncAuth";
-import * as fs from "fs"
+import setAppSyncAuth from './setAppSyncAuth'
+import * as fs from 'fs'
 
-const file = fs.readFileSync("outputs.json", {encoding: "utf-8"})
+const file = fs.readFileSync('outputs.json', { encoding: 'utf-8' })
 
-const json = JSON.parse(file);
-let stack: any;
+const json = JSON.parse(file)
+let stack: any
 
-stack = json[Object.keys(json)[0]];
+stack = json[Object.keys(json)[0]]
 
-stack.oauth = JSON.parse(stack.oauth);
-stack.functionMap = JSON.parse(stack.functionMap);
+stack.oauth = JSON.parse(stack.oauth)
+stack.functionMap = JSON.parse(stack.functionMap)
 const functionMap = Object.keys(stack.functionMap).map(key => {
-    return {
-        ...stack.functionMap[key],
-        endpoint: stack.functionMap[key].endpoint.substring(0, stack.functionMap[key].endpoint.length - 1) // Remove / from endpoint 
-    }
-});
+  return {
+    ...stack.functionMap[key],
+    endpoint: stack.functionMap[key].endpoint.substring(
+      0,
+      stack.functionMap[key].endpoint.length - 1
+    ), // Remove / from endpoint
+  }
+})
 
-setAppSyncAuth(stack.awsuserpoolsid, stack.awsuserpoolswebclientid, stack.outputAppSyncId, stack.outputCreateBatch, stack.tablenamemap, stack.tableArns)
+setAppSyncAuth(
+  stack.awsuserpoolsid,
+  stack.awsuserpoolswebclientid,
+  stack.outputAppSyncId,
+  stack.outputCreateBatch,
+  stack.tablenamemap,
+  stack.tableArns
+)
 
-fs.writeFileSync("../frontend/src/exports.js", `
+fs.writeFileSync(
+  '../frontend/src/exports.js',
+  `
 const exports = {
     aws_project_region: "eu-central-1",
     aws_cognito_region: "eu-central-1",
@@ -62,7 +74,8 @@ const exports = {
     "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
 }
 export default exports;
-`);
+`
+)
 
 // awsconfig.aws_cognito_identity_pool_id = outputs.awscognitoidentitypoolid;
 // awsconfig.aws_user_pools_id = outputs.awsuserpoolsid;
