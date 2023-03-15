9a4fc1beae2623792081f623ef2365e1783766fa
Vidarmic <vidarmic@stud.ntnu.no>
2023-03-15 08:24:55 +0100

    Remove owner from organization types

--- frontend/src/api/organizations/types.ts
+++ frontend/src/api/organizations/types.ts
@@ -5,7 +5,7 @@ export interface Organization {
   identifier_attribute: string
 }
 
-export type OrganizationInput = Omit<Organization, 'created_at' | 'owner'>
+export type OrganizationInput = Omit<Organization, 'created_at'>
 
 export type GetOrganizationInput = Pick<Organization, 'id'>
 export type DeleteOrganizationInput = Pick<Organization, 'id'>
