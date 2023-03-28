8fa4276d3b69dd89702ae25403893aa52f62cce2
Synnøve Soon Halle <synnove.halle@knowit.no>
2023-03-28 09:55:57 +0200

    Add node polyfill

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 import svgrPlugin from 'vite-plugin-svgr'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [
+    react(), 
+    viteTsconfigPaths(), 
+    svgrPlugin(),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
   server: {
-    port: 8000,
+    port: 3000,
   },
 })
