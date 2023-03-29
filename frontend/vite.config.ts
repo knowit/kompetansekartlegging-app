a45e8d60c4253e7efeb58e785d829d1f64862569
Synnøve Soon Halle <synnove.halle@knowit.no>
2023-03-29 10:26:17 +0200

    Replace react-scripts linting rules with custom, fix warnings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,8 +7,8 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(), 
-    viteTsconfigPaths(), 
+    react(),
+    viteTsconfigPaths(),
     svgrPlugin(),
     nodePolyfills({
       // Whether to polyfill `node:` protocol imports.
