7fe9503812553e2a5b2cc417fa4e20bd2e2b67bf
Synnøve Soon Halle <synnove.halle@knowit.no>
2023-04-09 16:52:55 +0200

    Add minor theming to show customization options

--- frontend/src/theme.tsx
+++ frontend/src/theme.tsx
@@ -1,9 +1,38 @@
-import {
-  unstable_createMuiStrictModeTheme as createMuiTheme,
-  adaptV4Theme,
-} from '@mui/material/styles'
+import { createTheme } from '@mui/material'
 import { KnowitColors } from './styles'
 
-const theme = createMuiTheme(adaptV4Theme({}))
+// See: https://mui.com/material-ui/customization/palette/
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: KnowitColors.greyGreen,
+      main: KnowitColors.darkBrown,
+      dark: KnowitColors.black,
+    },
+    secondary: {
+      light: KnowitColors.lightGreen,
+      main: KnowitColors.green,
+      dark: KnowitColors.darkGreen,
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        text: {
+          color: KnowitColors.darkGreen,
+        },
+        contained: {
+          color: KnowitColors.darkBrown,
+          backgroundColor: KnowitColors.lightGreen,
+          '&:hover': {
+            color: KnowitColors.white,
+            backgroundColor: KnowitColors.darkGreen,
+          },
+        },
+      },
+    },
+  },
+})
 
 export default theme
