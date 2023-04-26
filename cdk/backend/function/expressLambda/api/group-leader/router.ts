961ec43f9fbca4f70cfdefa412f431bf4b7622b3
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-04-26 15:52:02 +0200

    add possibility to search group members by group leaders as well

--- cdk/backend/function/expressLambda/api/group-leader/router.ts
+++ cdk/backend/function/expressLambda/api/group-leader/router.ts
@@ -2,8 +2,37 @@ import express from 'express'
 import { getUser } from '../cognito/cognitoActions'
 import { getUserOnRequest } from '../utils'
 import GroupLeader from './queries'
-import { GetByUsername, GetByUsernameAndOrganizationId } from './types'
+import { GetByUsername } from './types'
 const router = express.Router()
+
+const getAnnotatedGroupMembersByGroupLeader = async ({
+  username,
+}: GetByUsername) => {
+  const membersInGroup = await GroupLeader.getGroupMembersByGroupLeader({
+    username,
+  })
+
+  if (membersInGroup.status !== 'ok') {
+    throw new Error('Could not fetch group members')
+  }
+
+  // Annotate the members with Cognito User Pool data
+  const annotatedMembers = await Promise.all(
+    membersInGroup.data.map(
+      async (member: {
+        username: string
+        group_id: string
+        group_leader_username: string
+      }) => {
+        const { username } = member
+        const { UserAttributes } = await getUser(username)
+        return { ...member, cognitoAttributes: UserAttributes }
+      }
+    )
+  )
+
+  return annotatedMembers
+}
 
 router.get('/mygroup', async (req, res, next) => {
   try {
@@ -13,26 +42,9 @@ router.get('/mygroup', async (req, res, next) => {
       throw new Error('No username found on request')
     }
 
-    const membersInMyGroup = await GroupLeader.myGroupMembers({ username })
-
-    if (membersInMyGroup.status !== 'ok') {
-      throw new Error('Could not fetch group members')
-    }
-
-    // Annotate the members with Cognito User Pool data
-    const annotatedMembers = await Promise.all(
-      membersInMyGroup.data.map(
-        async (member: {
-          username: string
-          group_id: string
-          group_leader_username: string
-        }) => {
-          const { username } = member
-          const { UserAttributes } = await getUser(username)
-          return { ...member, cognitoAttributes: UserAttributes }
-        }
-      )
-    )
+    const annotatedMembers = await getAnnotatedGroupMembersByGroupLeader({
+      username,
+    })
 
     res.status(200).json({
       status: 'ok',
@@ -44,6 +56,28 @@ router.get('/mygroup', async (req, res, next) => {
     next(err)
   }
 })
+
+router.get<unknown, unknown, unknown, GetByUsername>(
+  '/group/:username',
+  async (req, res, next) => {
+    try {
+      const { username } = req.query
+
+      const annotatedMembers = await getAnnotatedGroupMembersByGroupLeader({
+        username,
+      })
+
+      res.status(200).json({
+        status: 'ok',
+        message: `🚀 ~ > Group members with cognito info for '${username}'`,
+        data: annotatedMembers,
+      })
+    } catch (error) {
+      console.error(error)
+      next(error)
+    }
+  }
+)
 
 router.get<unknown, unknown, unknown, GetByUsername>(
   '/lastanswerat/:username',
