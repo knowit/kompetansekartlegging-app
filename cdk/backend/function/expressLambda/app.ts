141135249f2020121af18d0af5ce04980a18294e
Christoffer Synnestvedt <christoffer.synnestvedt@gmail.com>
2023-05-05 20:21:09 +0200

    Make function sqlQuery generic

--- cdk/backend/function/expressLambda/app.ts
+++ cdk/backend/function/expressLambda/app.ts
@@ -29,48 +29,56 @@ const cmdConstants = {
   secretArn: process.env.SECRET_ARN,
   database: process.env.DATABASE_NAME,
 }
-interface SqlQueryInput {
+
+interface SqlQueryInput<T> {
   query: string
   parameters?: SqlParameter[]
   message: string
+  isArray?: boolean
 }
-export const sqlQuery = async ({
+
+type SqlQueryResult<T> = {
+  status: string
+  message: string
+  data: T | null
+}
+
+export const sqlQuery = async <T>({
   query,
   parameters,
   message,
-}: SqlQueryInput) => {
+  isArray = false,
+}: SqlQueryInput<T>): Promise<SqlQueryResult<T>> => {
   const cmd = new ExecuteStatementCommand({
     sql: query,
     parameters,
     ...cmdConstants,
     formatRecordsAs: RecordsFormatType.JSON,
   })
+
   const response = await rds.send(cmd)
 
+  return handleResponse<T>(response, message, isArray)
+}
+
+const handleResponse = <T>(
+  response: any,
+  message: string,
+  isArray: boolean
+): SqlQueryResult<T> => {
   const status =
-    response.$metadata.httpStatusCode &&
     response.$metadata.httpStatusCode >= 200 &&
     response.$metadata.httpStatusCode < 300
       ? 'ok'
       : 'unknown'
 
-  console.log('🚀 ~ response', response)
   if (response.formattedRecords) {
-    const records = JSON.parse(response.formattedRecords)
-    if (records.length === 0) {
-      return {
-        status,
-        message: '🧨 ~ No records returned from the query.',
-        data: null,
-      }
-    }
-    return {
-      status,
-      message,
-      data: records.length === 1 ? records[0] : records,
-    }
-  } else {
-    throw new Error(`Something went wrong in sqlQuery(...)`)
+    const records = isArray
+      ? (JSON.parse(response.formattedRecords) as T)
+      : (JSON.parse(response.formattedRecords)[0] as T)
+    return { status, message, data: records || null }
   }
+
+  throw new Error('Something went wrong in sqlQuery(...)')
 }
 export default app
