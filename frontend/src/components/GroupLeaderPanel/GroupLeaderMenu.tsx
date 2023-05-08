3474df7fb9925fff25f30ecc131d2041c52385fb
Synnøve Soon Halle <synnove.halle@knowit.no>
2023-05-08 08:43:45 +0200

    Add overview as submenu item

--- frontend/src/components/GroupLeaderPanel/GroupLeaderMenu.tsx
+++ frontend/src/components/GroupLeaderPanel/GroupLeaderMenu.tsx
@@ -1,8 +1,12 @@
 import { getAttribute } from '../AdminPanel/helpers'
-const getGroupMenuitems = (members: any) =>
-  members.map((m: any) => ({
+const getGroupMenuitems = (members: any) => {
+  const items = members.map((m: any) => ({
     text: getAttribute(m, 'name'),
     key: m.Username,
   }))
 
+  items.unshift({ text: 'menu.overview', key: 'MAIN' })
+  console.log(items)
+  return items
+}
 export default getGroupMenuitems
