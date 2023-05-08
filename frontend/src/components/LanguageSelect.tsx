c9a1bd9836ae31afcba1233409c865f183703d4e
Synnøve Soon Halle <synnove.halle@knowit.no>
2023-05-08 09:56:35 +0200

    Style language select

--- frontend/src/components/LanguageSelect.tsx
+++ frontend/src/components/LanguageSelect.tsx
@@ -4,6 +4,18 @@ import { availableLanguages } from '../i18n/i18n'
 import { I18n as amplifyI18n } from 'aws-amplify'
 import LanguageIcon from '@mui/icons-material/Language'
 import { KnowitColors } from '../styleconstants'
+import styled from '@emotion/styled'
+
+const StyledContainer = styled.div`
+  .languageSelect {
+    ${(props) => props.color && `border-color: ${props.color}`};
+    height: min-content;
+    box-shadow: none;
+    > * {
+      border: 0;
+    }
+  }
+`
 
 type LanguageSelectProps = {
   color?: string
@@ -19,37 +31,40 @@ export const LanguageSelect = ({ color }: LanguageSelectProps) => {
   }
 
   return (
-    <Select
-      value={i18n.language}
-      onChange={(event) => changeLanguage(event.target.value as string)}
-      renderValue={() => <LanguageIcon style={{ color: color }} />}
-      aria-label={
-        i18n.t('aria.selectLanguageLanguageIsSelected', {
-          language: availableLanguages[i18n.language],
-        }) as string
-      }
-    >
-      {Object.keys(availableLanguages).map((language) => (
-        <MenuItem
-          key={language}
-          value={language}
-          style={
-            i18n.language == language
-              ? { backgroundColor: '#e4e0dc' }
-              : { backgroundColor: 'transparent' }
-          }
-        >
-          <div
-            style={{
-              fontSize: 14,
-              fontWeight: 'bold',
-              color: KnowitColors.darkBrown,
-            }}
+    <StyledContainer>
+      <Select
+        className="languageSelect"
+        value={i18n.language}
+        onChange={(event) => changeLanguage(event.target.value as string)}
+        renderValue={() => <LanguageIcon style={{ color: color }} />}
+        aria-label={
+          i18n.t('aria.selectLanguageLanguageIsSelected', {
+            language: availableLanguages[i18n.language],
+          }) as string
+        }
+      >
+        {Object.keys(availableLanguages).map((language) => (
+          <MenuItem
+            key={language}
+            value={language}
+            style={
+              i18n.language == language
+                ? { backgroundColor: '#e4e0dc' }
+                : { backgroundColor: 'transparent' }
+            }
           >
-            {availableLanguages[language]}
-          </div>
-        </MenuItem>
-      ))}
-    </Select>
+            <div
+              style={{
+                fontSize: 14,
+                fontWeight: 'bold',
+                color: KnowitColors.darkBrown,
+              }}
+            >
+              {availableLanguages[language]}
+            </div>
+          </MenuItem>
+        ))}
+      </Select>
+    </StyledContainer>
   )
 }
