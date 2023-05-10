df1b1ff6788e421073af7697ee71e40f5b1a3b39
Christoffer Synnestvedt <christoffer.synnestvedt@gmail.com>
2023-05-10 14:32:50 +0200

    Add AddUsersToGroupDialog to rewritten group

---
+++ frontend/src/components/AdminPanel/rewritten-group/dialogs/AddUsersToGroupDialog.tsx
@@ -0,0 +1,267 @@
+import { useEffect, useState } from 'react'
+
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Chip from '@mui/material/Chip'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormGroup from '@mui/material/FormGroup'
+import IconButton from '@mui/material/IconButton'
+import Paper from '@mui/material/Paper'
+import Switch from '@mui/material/Switch'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TextField from '@mui/material/TextField'
+import { makeStyles } from '@mui/styles'
+
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogTitle from '@mui/material/DialogTitle'
+
+import { Close as CloseIcon } from '@mui/icons-material'
+import { Table, TableRow } from '@mui/material'
+import { useQuery } from '@tanstack/react-query'
+import { useTranslation } from 'react-i18next'
+import { getAllUsers } from '../../../../api/admin'
+import { IUserAnnotated } from '../../../../api/admin/types'
+import { selectUserState } from '../../../../redux/User'
+import { useAppSelector } from '../../../../redux/hooks'
+import { dialogStyles } from '../../../../styles'
+import PictureAndNameCell from '../../PictureAndNameCell'
+import { containsUser, getAttribute } from '../utils'
+
+const getNameOrUsername = (user: any) => {
+  const name = getAttribute(user, 'name')
+  return name || user.Username
+}
+
+const AddMemberToGroupDialog = ({
+  onCancel,
+  onConfirm,
+  open,
+  members,
+}: any) => {
+  const { t } = useTranslation()
+  const style = dialogStyles()
+  const userState = useAppSelector(selectUserState)
+
+  const [showOnlyUnset, setShowOnlyUnset] = useState<boolean>(true)
+  const [nameFilter, setNameFilter] = useState<string>('')
+  const [selectedUsers, setSelectedUsers] = useState<IUserAnnotated[]>([])
+  const [usersInList, setUsersInList] = useState<IUserAnnotated[]>([])
+
+  const onSelect = (user: IUserAnnotated) => {
+    if (selectedUsers.find((u) => user.username === u.username)) {
+      setSelectedUsers(
+        selectedUsers.filter((u) => user.username !== u.username)
+      )
+    } else {
+      setSelectedUsers([...selectedUsers, user])
+    }
+  }
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['get-all-users'],
+    queryFn: getAllUsers,
+  })
+
+  const onClose = () => {
+    setSelectedUsers([])
+    onCancel()
+  }
+
+  const toggleShowOnlyUnset = () =>
+    setShowOnlyUnset((showOnlyUnset) => !showOnlyUnset)
+
+  const nameFilterFn = (user: IUserAnnotated) => {
+    const name = getAttribute(user, 'name')
+    return (
+      !name ||
+      name.toLocaleLowerCase().startsWith(nameFilter.toLocaleLowerCase())
+    )
+  }
+  const showOnlyUnsetFilterFn = (user: IUserAnnotated) =>
+    !showOnlyUnset || !user.group_leader_username
+
+  useEffect(() => {
+    const usersToShow = data?.data
+      ?.filter((user) => !containsUser(user, members))
+      .filter(nameFilterFn)
+      .filter(showOnlyUnsetFilterFn)
+    setUsersInList(usersToShow ?? [])
+  }, [data?.data])
+
+  console.log(usersInList)
+  console.log(data?.data)
+
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      fullWidth
+      maxWidth="sm"
+      PaperProps={{
+        style: { borderRadius: 30 },
+      }}
+    >
+      <DialogTitle>
+        <Box
+          component="div"
+          mb={1}
+          display="flex"
+          justifyContent="space-between"
+        >
+          <span className={style.dialogTitleText}>
+            {t('myGroup.addMembers')}
+          </span>
+          <IconButton
+            className={style.closeButton}
+            onClick={onCancel}
+            size="large"
+          >
+            <CloseIcon />
+          </IconButton>
+        </Box>
+        <FormGroup row>
+          <TextField
+            fullWidth
+            placeholder={
+              t('searchForEmployeeInOrganization', {
+                organization: userState.organizationName,
+              }) as string
+            }
+            variant="outlined"
+            value={nameFilter}
+            className={style.searchField}
+            onChange={(e: any) => setNameFilter(e.target.value)}
+            helperText={t('myGroup.theEmployeeMustHaveSignedInOnce')}
+          />
+          <FormControlLabel
+            control={
+              <Switch
+                checked={showOnlyUnset}
+                onChange={toggleShowOnlyUnset}
+                color="primary"
+              />
+            }
+            label={t('myGroup.showOnlyEmployeesWithoutGroupLeader')}
+          />
+        </FormGroup>
+        {selectedUsers.length > 0 && (
+          <SelectedUsers
+            selectedUsers={selectedUsers}
+            setSelectedUser={onSelect}
+          />
+        )}
+      </DialogTitle>
+      <DialogContent style={{ maxHeight: '512px' }}>
+        <UsersTable
+          users={usersInList}
+          selectedUsers={selectedUsers}
+          setSelectedUser={onSelect}
+        />
+      </DialogContent>
+      <DialogActions className={style.alertButtons}>
+        <Button onClick={onClose} className={style.cancelButton}>
+          <span className={style.buttonText}>{t('abort')}</span>
+        </Button>
+        <Button
+          onClick={() => {
+            onConfirm(selectedUsers)
+            onClose()
+          }}
+          disabled={selectedUsers.length === 0}
+          className={style.confirmButton}
+        >
+          <span className={style.buttonText}>{t('add')}</span>
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+const useStylesSelectedUsers = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    listStyle: 'none',
+    padding: theme.spacing(0.5),
+    margin: 0,
+  },
+  chip: {
+    margin: theme.spacing(0.5),
+  },
+}))
+
+const SelectedUsers = ({ selectedUsers, setSelectedUser }: any) => {
+  const classes = useStylesSelectedUsers()
+  return (
+    <Paper component="ul" className={classes.root}>
+      {selectedUsers.map((user: IUserAnnotated) => {
+        const nameOrUsername = getNameOrUsername(user)
+        return (
+          <li key={user.username}>
+            <Chip
+              label={nameOrUsername}
+              onDelete={() => setSelectedUser(user)}
+              className={classes.chip}
+            />
+          </li>
+        )
+      })}
+    </Paper>
+  )
+}
+
+const User = ({ user, selected, setSelectedUser }: any) => {
+  const { t } = useTranslation()
+  const name = getAttribute(user, 'name')
+  const picture = getAttribute(user, 'picture')
+  const hasGroup = !!user.groupLeader
+  const groupLeaderName = hasGroup && getAttribute(user.groupLeader, 'name')
+
+  return (
+    <>
+      <TableRow hover selected={selected} onClick={() => setSelectedUser(user)}>
+        <TableCell>
+          <PictureAndNameCell name={name} picture={picture} />
+        </TableCell>
+        <TableCell>{groupLeaderName || t('myGroup.noGroupLeader')}</TableCell>
+      </TableRow>
+    </>
+  )
+}
+
+const UsersTable = ({ users, selectedUsers, setSelectedUser }: any) => {
+  const { t } = useTranslation()
+  const isSelected = (user: any) =>
+    selectedUsers.some((u: any) => u.Username === user.Username)
+
+  return (
+    <TableContainer component={Paper} style={{ height: '100%' }}>
+      <Table stickyHeader style={{ maxHeight: '100%' }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>{t('employee')}</TableCell>
+            <TableCell>{t('groupLeader')}</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((u: any) => (
+            <User
+              key={u.Username}
+              user={u}
+              selected={isSelected(u)}
+              setSelectedUser={setSelectedUser}
+            />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+export default AddMemberToGroupDialog
