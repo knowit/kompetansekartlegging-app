c3ac1557dabf1ddf401c21735dd4fbd88ab190c2
Christoffer Synnestvedt <christoffer.synnestvedt@gmail.com>
2023-06-22 09:55:13 +0200

    Remove moved code

--- cdk/backend/function/expressLambda/api/catalog/router.ts
+++ cdk/backend/function/expressLambda/api/catalog/router.ts
@@ -1,12 +1,6 @@
 import express from 'express'
 import { getOrganizations } from '../utils'
 import Catalog from './queries'
-import {
-  CatalogInput,
-  DeleteCatalogInput,
-  GetCatalogInput,
-  UpdateCatalogInput,
-} from './types'
 
 const router = express.Router()
 
@@ -24,47 +18,5 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
-
-// Create
-router.post<unknown, unknown, CatalogInput>('/', async (req, res, next) => {
-  try {
-    const createResponse = await Catalog.createCatalog(req.body)
-
-    res.status(200).json(createResponse)
-  } catch (err) {
-    console.error(err)
-    next(err)
-  }
-})
-
-// Delete
-router.delete<unknown, unknown, DeleteCatalogInput>(
-  '/',
-  async (req, res, next) => {
-    try {
-      const deleteResponse = await Catalog.deleteCatalog(req.body)
-
-      res.status(200).json(deleteResponse)
-    } catch (err) {
-      console.error(err)
-      next(err)
-    }
-  }
-)
-
-// Update
-router.patch<unknown, unknown, UpdateCatalogInput, GetCatalogInput>(
-  '/',
-  async (req, res, next) => {
-    try {
-      const updateResponse = await Catalog.updateCatalog(req.query, req.body)
-
-      res.status(200).json(updateResponse)
-    } catch (error) {
-      console.error(error)
-      next(error)
-    }
-  }
-)
 
 export { router as catalogRouter }
