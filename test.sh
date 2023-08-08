7bb25e5c2b169b83c4b89efe520a4698d8db9faa
Anders Rebner <anders.rebner@knowit.no>
2023-08-08 14:39:45 +0200

    Simplifies root test script

--- test.sh
+++ test.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-echo "Installing python dependencies..."
-requirement_files=$(find . -name 'requirements.txt')
-for requirement_file in $requirement_files;
-do
-    if [[ $requirement_file != *'node_modules'* && $requirement_file != *'cdk.out'* ]]; then
-        pip install -q -r $requirement_file
-    fi
-done
-
-echo "Running python tests..."
-pytest
-
-echo "Installing ./cdk dependencies..."
-cd cdk && npm install -s
-echo "Installing ./cdk/backend/function/AdminQueries dependencies..."
-cd backend/function/AdminQueries && npm install -s
-
 echo "Running ./cdk tests..."
-cd ../../../ && npm run test
+cd cdk && ./test.sh install-dependencies && cd ..
