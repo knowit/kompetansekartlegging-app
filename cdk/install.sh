673b7f86f3cec6f5bced5a0c7c9a3b09d7fe7349
Anders Rebner <anders.rebner@knowit.no>
2023-08-08 15:28:53 +0200

    Install python dependencies in cdk/install.sh

--- cdk/install.sh
+++ cdk/install.sh
@@ -4,3 +4,13 @@ cd backend/presignup && npm install
 cd ../function/AdminQueries && npm install
 cd ../createUserformBatch && npm install
 cd ../externalAPI && npm install
+
+# Install python packages
+cd ../..
+requirement_files=$(find . -name 'requirements.txt')
+for requirement_file in $requirement_files;
+do
+    if [[ $requirement_file != *'node_modules'* && $requirement_file != *'cdk.out'* ]]; then
+        pip install -q -r $requirement_file
+    fi
+done
